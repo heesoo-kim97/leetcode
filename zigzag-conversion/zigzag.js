786b4fe7355cc80a5e01bc07236243a2d17c9fde
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-19 06:21:19 +0000

    added convert and changed the function to arrow function.

--- zigzag-conversion/zigzag.js
+++ zigzag-conversion/zigzag.js
@@ -1,1 +1,12 @@
 /* exported zigzagConversion */
+
+const convert = (s, numRows) => {
+  if (s.length <= numRows || numRows < 2) {
+    return s;
+  }
+  // const len = s.length;
+  // const num = 2 * (numRows - 1);
+};
+
+// eslint-disable-next-line no-console
+console.log(convert);
