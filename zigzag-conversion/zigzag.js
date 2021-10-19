15573e974e90dece982e063395aac68912d5b87b
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-19 06:24:01 +0000

    Completed zigzag conversion leetcode exercise.

--- zigzag-conversion/zigzag.js
+++ zigzag-conversion/zigzag.js
@@ -4,8 +4,19 @@ const convert = (s, numRows) => {
   if (s.length <= numRows || numRows < 2) {
     return s;
   }
-  // const len = s.length;
-  // const num = 2 * (numRows - 1);
+  const len = s.length;
+  const num = 2 * (numRows - 1);
+  const res = Array(numRows).fill('');
+  let tmp = 0;
+  for (let i = 0; i < len; i++) {
+    tmp = i % num;
+    if (tmp < numRows) {
+      res[tmp] += s[i];
+    } else {
+      res[num - tmp] += s[i];
+    }
+  }
+  return res.join('');
 };
 
 // eslint-disable-next-line no-console
