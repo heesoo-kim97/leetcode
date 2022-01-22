d765a72201ddcbc4e1584a49b9e2d4fdb415e925
heesoo-kim97 <heesoo0323@gmail.com>
2022-01-22 07:06:05 +0000

    another way to add total.

--- two-sum/sum.js
+++ two-sum/sum.js
@@ -7,7 +7,8 @@ function twoSum(num1, num2) {
 
 // eslint-disable-next-line no-unused-vars
 function twoSum2(num3, num4) {
-  return num3 + num4;
+  const total = num3 + num4;
+  return total;
 }
 
 // adding two integers and return the total value
