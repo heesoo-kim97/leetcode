d7438a2701ad93754b7711ebdd3dca9be66eb510
heesoo-kim97 <heesoo0323@gmail.com>
2022-01-22 07:05:15 +0000

    added second function.

--- two-sum/sum.js
+++ two-sum/sum.js
@@ -1,8 +1,13 @@
-/* exported twoSum */
+/* exported twoSum twoSum2 */
 
 // eslint-disable-next-line no-unused-vars
 function twoSum(num1, num2) {
   return num1 + num2;
+}
+
+// eslint-disable-next-line no-unused-vars
+function twoSum2(num3, num4) {
+  return num3 + num4;
 }
 
 // adding two integers and return the total value
